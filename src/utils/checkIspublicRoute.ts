7b3609ae06716629ce9d37b41b42ab8d470f91be
serafim <matheus18serafim@gmail.com>
2023-11-13 15:31:17 -0300

    REMOVE: utils

--- src/utils/checkIspublicRoute.ts
+++
@@ -1,6 +0,0 @@
-import { APP_ROUTES } from "./routes";
-
-export const checkIsPublicRoute = (pathname: string) => {
-  const isPublicRoute = Object.values(APP_ROUTES.public).includes(pathname);
-  return isPublicRoute;
-};
